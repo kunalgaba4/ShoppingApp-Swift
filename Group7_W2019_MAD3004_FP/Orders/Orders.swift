c11c7bd61ebc753c85b0d032c60321b9c81ab5b0
sumit kumar <imsumitk1@gmail.com>
2019-02-16 10:04:12 -0800

    Added Orders group and Shipping File

---
+++ Group7_W2019_MAD3004_FP/Orders/Orders.swift
@@ -0,0 +1,24 @@
+//
+//  Orders.swift
+//  Group7_W2019_MAD3004_FP
+//
+//  Created by sumit kumar on 16/02/19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+
+class Orders{
+    orderid: Int
+    dateCreated: String
+    dateShipped: String
+    customerName: String
+    customerId: String
+    status: String
+    shippingId: String
+    
+    placeOrder(){
+    
+    }
+}
---
+++ Group7_W2019_MAD3004_FP/ShoppingCart.swift
@@ -0,0 +1,32 @@
+//
+//  ShoppingCart.swift
+//  Group7_W2019_MAD3004_FP
+//
+//  Created by sumit kumar on 16/02/19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingCart{
+    cartId: Int
+    productId: Int
+    quantity: Int
+    dateAdded: Int
+    
+    addCartItem(){
+    
+    }
+    
+    updateQuantity(){
+    
+    }
+    
+    viewCartDetails(){
+    
+    }
+    
+    checkOut(){
+    
+    }
+}
